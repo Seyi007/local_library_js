97cdf879a5d5f6c25ffc3de6b42fe8ad368b34c3
Seyi <kuforiji98@gmail.com>
2023-09-10 12:37:11 +0100

    adding models schema to authors and book models file

--- local-library-exp/models/author.js
+++ local-library-exp/models/author.js
@@ -4,8 +4,27 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const AuthorSchema = new Schema({
-    first_name: { type: String, required: true, maxLength: 100},
-    family_name: { type: String, required: true, maxLength: 100},
+    first_name: { type: String, required: true, maxLength: 100 },
+    family_name: { type: String, required: true, maxLength: 100 },
     date_of_birth: { type: Date },
     date_of_death: { type: Date },
-})
+});
+
+// Virtual for author's full name
+AuthorSchema.virtual("name").get(function () {
+    // To avoid errors in cases where an author does not have either a family name or first name
+    // We want to make sure we handle the exception by returning an empty string for that case
+    let fullname = "";
+    if (this.first_name && this.family_name) {
+        fullname = `${this.first_name}, ${this.family_name}`;
+    }
+    return fullname;
+});
+
+// Virtual for author's url
+AuthorSchema.virtual("url").get(function () {
+    return `catalog/author/${this._id}`;
+});
+
+// Export model
+module.exports = mongoose.model("Author", AuthorSchema);
